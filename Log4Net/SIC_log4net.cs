2de52479bd1b6915eff7f73258b22d1aafc4abd7
占超 吴 <zhanchao.wu@icloud.com>
2018-11-14 15:10:41 +0800

    log4net

--- Log4Net/SIC_log4net.cs
+++ Log4Net/SIC_log4net.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 namespace Sephiroth.Infrastructure.Common.Log4Net
 {
-
+    /// <summary>
+    /// 
+    /// </summary>
     public class SIC_log4net
 	{
 		/// <summary>
@@ -24,16 +26,50 @@ namespace Sephiroth.Infrastructure.Common.Log4Net
 		/// </summary>
 		/// <param name="t"></param>
 		/// <param name="msg"></param>
+        /// <param name="messageType"></param>
 		#region static void WriteLog(Type t, string msg)
 
-		public static void WriteLog(Type t, string msg)
+		public static void WriteLog(Type t, string msg, MessageType messageType = MessageType.Info)
 		{
 			log4net.ILog log = log4net.LogManager.GetLogger(t);
-			log.Error(msg);
-		}
+            if (messageType == MessageType.Error)
+                log.Error(msg);
+            else if (messageType == MessageType.Fatal)
+                log.Fatal(msg);
+            else if (messageType == MessageType.Debug)
+                log.Debug(msg);
+            else if (messageType == MessageType.Info)
+                log.Info(msg);
+            else if (messageType == MessageType.Warn)
+                log.Warn(msg);
+        }
 
 		#endregion
 
 
 	}
+
+    public enum MessageType
+    {
+        /// <summary>
+        /// 错误
+        /// </summary>
+        Error,
+        /// <summary>
+        /// 调试
+        /// </summary>
+        Debug,
+        /// <summary>
+        /// 重大
+        /// </summary>
+        Fatal,
+        /// <summary>
+        /// 信息
+        /// </summary>
+        Info,
+        /// <summary>
+        /// 警告
+        /// </summary>
+        Warn
+    }
 }
